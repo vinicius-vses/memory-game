d43e91c97c577184908de3cabe7fdc5a8d1f1b0c
Vinicius Souza <viniciussouzaesp@gmail.com>
2024-12-15 10:09:35 -0300

    Add emojis nos cartões

---
+++ src/scripts/engine.js
@@ -0,0 +1,34 @@
+const emojis = [
+  "😎",
+  "😎",
+  "❤️",
+  "❤️",
+  "🤔",
+  "🤔",
+  "🤨",
+  "🤨",
+  "🤑",
+  "🤑",
+  "💀",
+  "💀",
+  "👾",
+  "👾",
+  "🐺",
+  "🐺",
+  "🐉",
+  "🐉",
+  "🦈",
+  "🦈",
+];
+
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i < emojis.length; i++) {
+  let box = document.createElement("div");
+  box.className = "item";
+  box.innerHTML = shuffleEmojis[i];
+  document.querySelector(".game").appendChild(box);
+
+}
